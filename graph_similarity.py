3f74a8c8903443c3792760e52fe819463588dcf1
Vijin Kotta Parambath <vparambath@rhythmone.com>
2018-11-07 19:25:09 +0530

    changesto write kernel output in csv format

--- graph_similarity.py
+++ graph_similarity.py
@@ -1,7 +1,6 @@
 import numpy as np
 import graphkernels.kernels as gk
 from os import listdir
-from os.path import join
 from igraph import *
 from sklearn.externals import joblib
 import os
@@ -13,7 +12,14 @@ def get_igraph(file, master_map):
 	return graph
 
 def get_graphs(year, folder_path, master_map):
-	return ([get_igraph(join(folder_path,file), master_map) for file in listdir(folder_path) if '.net' in file] , ['{0}_{1}'.format(year, file.split('.')[0]) for file in listdir(folder_path) if '.net' in file])
+	return ([get_igraph(os.path.join(folder_path,file), master_map) for file in listdir(folder_path) if '.net' in file] , ['{0}_{1}'.format(year, file.split('.')[0]) for file in listdir(folder_path) if '.net' in file])
+
+def create_kernel_csv(kernel, row_labels, col_labels, file_path):
+	str_kernel = np.char.mod('%d', kernel)
+	csv_str = ',' + ','.join(col_labels)+ '\n'+ '\n'.join([row_label[index]+ ',' + ','.join(x) for index, x in enumerate(kernel)])
+	with open(file_path, 'w') as fp:
+		fp.write(csv_str)
+
 
 if __name__ == '__main__':
 	root_folder = '/home/hduser/iit_data/ask_ubuntu/year_wise_communities'
@@ -30,8 +36,8 @@ if __name__ == '__main__':
 
 	for yr1, yr2 in year_tup:
 		print(yr1, yr2)
-		yr1_graphs , yr1_labels = get_graphs(yr1, join(root_folder, str(yr1), 'communities_pajek'), master_map)
-		yr2_graphs , yr2_labels = get_graphs(yr2, join(root_folder, str(yr2), 'communities_pajek'), master_map)
+		yr1_graphs , yr1_labels = get_graphs(yr1, os.path.join(root_folder, str(yr1), 'communities_pajek'), master_map)
+		yr2_graphs , yr2_labels = get_graphs(yr2, os.path.join(root_folder, str(yr2), 'communities_pajek'), master_map)
 
 		graph_list = []
 		graph_list.extend(yr1_graphs)
@@ -40,6 +46,7 @@ if __name__ == '__main__':
 		K_WL = gk.CalculateWLKernel(graph_list, par = 3)
 		K_WL_subset = K_WL[:len(yr1_graphs), len(yr1_graphs):]
 		joblib.dump(K_WL_subset, '{0}/WL-{1}-{2}.pkl'.format(outpath, yr1, yr2), compress = 1)
+		create_kernel_csv(K_WL_subset, yr1_labels, yr2_labels, '{0}/WL-{1}-{2}.csv'.format(outpath, yr1, yr2))
 
 		# only merge and one to one matching is possible, split is not possible
 		# split can be defined by setting a threshold which will help to identify if a community is matched with more than one on next time interval
