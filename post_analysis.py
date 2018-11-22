d0cf64f19922a7f7b0b75356b82ee443857ed5e2
vijinkp <kpvijin@gmail.com>
2018-11-23 01:43:10 +0530

    temporal data preparation

--- post_analysis.py
+++ post_analysis.py
@@ -4,6 +4,9 @@ from os import listdir
 import os
 import xml.etree.ElementTree
 import dateutil.parser
+import warnings
+
+warnings.filterwarnings('ignore')
 
 community_data_folder = '/home/hduser/iit_data/ask_ubuntu/models/temporal_chains'
 community_tags_folder = '/home/hduser/iit_data/ask_ubuntu/year_wise_communities'
@@ -33,55 +36,78 @@ def common_member(a, b):
         return(True)
     return(False)
 
-def get_question_data(year, tag_id, data):
-	file_path = os.path.join(community_tags_folder, year, 'community_tags', '{0}_tags.txt'.format(tag_id))
-	tags = None
-	with open(file_path , 'r') as fp:
-		tags = fp.read().split('\n')
+def get_tags(year, community_ids):
+	tag_list = []
+	for community_id in community_ids:
+		file_path = os.path.join(community_tags_folder, year, 'community_tags', '{0}_tags.txt'.format(community_id))
+		tags = None
+		with open(file_path , 'r') as fp:
+			tag_list.extend(fp.read().split('\n'))
+	return list(set(tag_list))
 
+
+def get_question_data(year, tags, data):
 	year_data = data[data['creation_year'] == year]
-	return year_data[year_data.tag_arr.apply(lambda x : common_member(x, tags))][['id','accepted_answer_id']] 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	return year_data[year_data.tag_arr.apply(lambda x : common_member(x, tags))][['Id','AcceptedAnswerId']] 
 
 if __name__ == '__main__':
 
-	input_schema = ['id', 'post_type_id', 'accepted_answer_id', 'creation_date', 'score', 'view_count', 'body','owner_user_id','lasteditor_user_id',
-	'lasteditor_display_name','lastedit_date','lastactivity_date' ,'title' ,'tags' ,'answer_count' ,'comment_count' ,'favorite_count' ,'community_owned_date' ]
+	# input_schema = ['AcceptedAnswerId', 'AnswerCount', 'Body', 'ClosedDate', 'CommentCount', 'CommunityOwnedDate', 'CreationDate', 'FavoriteCount', 'Id', 'LastActivityDate', 'LastEditDate', 'LastEditorDisplayName', 'LastEditorUserId', 'OwnerDisplayName', 'OwnerUserId', 'ParentId','PostTypeId', 'Score', 'Tags', 'Title', 'ViewCount']
 
-	user_data = pd.read_csv('/home/hduser/iit_data/ask_ubuntu/users.csv', sep='\u0001', engine='python')
+ 	global_sep = ','
+ 	user_data = pd.read_csv('/home/hduser/iit_data/ask_ubuntu/users.csv', sep='\u0001', engine='python')
+ 	data =  process_post_data('/home/hduser/iit_data/ask_ubuntu/Posts.xml', ['CreationDate'])
 
-	data =  process_post_data('/home/hduser/iit_data/ask_ubuntu/Posts.xml', ['CreationDate'])
+ 	# Cleaned tag set and year
+ 	data['creation_year'] = data.CreationDate.apply(lambda x : str(x.year))
+ 	data['cleaned_tags'] = data.Tags.apply(lambda x : str(x).replace("><", ",").replace(">", "").replace("<", "").strip())
 
-	# Cleaned tag set and year
-	data['creation_year'] = data.CreationDate.apply(lambda x : str(x.year))
-	data['cleaned_tags'] = data.Tags.apply(lambda x : str(x).replace("><", ",").replace(">", "").replace("<", "").strip())
+ 	# Quality questions
+ 	# score = upvotes - downvotes
+ 	qn_data = data[data.PostTypeId == '1']
+ 	qn_data.Score = qn_data.Score.astype('float')
+ 	quality_qn = qn_data[np.logical_and(qn_data.Score > 0, np.logical_not(qn_data.AcceptedAnswerId.isna()))]
+ 	quality_qn['tag_arr'] = quality_qn.cleaned_tags.apply(lambda x : x.split(','))
 
-	# Quality questions
-	# score = upvotes - downvotes
-	qn_data = data[data.PostTypeId == '1']
-	qn_data.Score = qn_data.Score.astype('float')
-	quality_qn = qn_data[np.logical_and(qn_data.Score > 0, qn_data.AcceptedAnswerId != 0)]
-	quality_qn['tag_arr'] = quality_qn.cleaned_tags.apply(lambda x : x.split(','))
+ 	# answer data
+ 	answer_data = data[data.ParentId != 'nan']
 
-	for file in listdir(community_data_folder):
-		with open(os.path.join(community_data_folder, file), 'r') as fp:
+ 	temporal_data = []
+ 	for file in listdir(community_data_folder):
+ 		print('Processing chain : {0}'.format(file))
+ 		with open(os.path.join(community_data_folder, file), 'r') as fp:
+ 			lines = [(x.strip().split('_')[0],x.strip().split('_')[1])  for x in fp.readlines()]
+ 		
+ 		year_dict = dict()
+ 		[year_dict[t[0]].append(t[1]) if t[0] in list(year_dict.keys()) else year_dict.update({t[0]: [t[1]]}) for t in lines]
+
+ 		for year, community_ids in year_dict.items():
+ 			tags = get_tags(year, community_ids)
+
+ 			# get questions for corresponding tags
+ 			qn_ans_data = get_question_data(year, tags, quality_qn[quality_qn.creation_year == year])
+
+ 			# find users
+ 			join_data = qn_ans_data.join(answer_data.set_index('Id', inplace=False), on='AcceptedAnswerId', rsuffix='_ans', how='left')
+ 			unique_users = list(join_data.OwnerUserId.unique())
+ 			user_size = len(unique_users)
+ 			qn_size = qn_ans_data.shape[0]
+
+ 			# user based profiles
+ 			# Sum of reputation scores of users
+ 			reputation_score = user_data[user_data.Id.isin(unique_users)].Reputation.sum()
+
+ 			# Number of answers provided by users on that year
+ 			answer_year_data = answer_data[answer_data.creation_year == year]
+ 			user_answer_count = answer_year_data[answer_year_data.OwnerUserId.isin(unique_users)].shape[0]
+ 			temporal_data.append(global_sep.join([file, year, ':'.join(tags), str(user_size), str(qn_size), str(reputation_score), str(user_answer_count)]))
+ 	
+ 	schema = ['chain', 'year', 'tags', 'no_users', 'no_questions', 'sum_reputation', 'user_answer_count']
+ 	with open('/home/hduser/iit_data/ask_ubuntu/models/temporal_data.csv', 'w') as fp:
+ 		fp.write(','.join(schema) +'\n'+('\n'.join(temporal_data)))
+
+
+
+
+
+
